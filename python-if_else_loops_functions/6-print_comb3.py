8c4301a6fc332746c50eed15bdefff3d28fc9569
Mottais <mottais.afy@outlook.fr>
2024-01-22 21:16:43 +0100

    Task6 Test autre solution

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for dec in range(10):
     for unit in range(dec, 10):
         if dec != unit:
             if not(dec == 8 and unit == 9):
-                print("{}{}, ".format(dec, unit), end=", ")
+                print("{}{}, ".format(dec, unit), end="")
 print("89")
