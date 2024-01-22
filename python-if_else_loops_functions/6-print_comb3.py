21b52fef4a8259b354c5bcb6f670da54d93346a7
Mottais <mottais.afy@outlook.fr>
2024-01-22 20:09:39 +0100

    Task6 Write a program that prints all possible different combinations of two digits.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for dec in range(10):
+    for unit in range(dec, 10):
+        if dec != unit:
+            if not(dec == 8 and unit == 9):
+                print(dec, unit, sep="", end=", ")
+print("89")
