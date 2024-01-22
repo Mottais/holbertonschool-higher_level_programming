31a416cf91df3cfe5e76efb54abae94c407fe491
Mottais <mottais.afy@outlook.fr>
2024-01-22 21:25:03 +0100

    Task6 Test autre solution

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(i, 10):
         if i != j:
             if i != 8 or j != 9:
-                print("{}{}, ".format(int(i), int(j)), end="")
+                print("{}{}, ".format(i, j), end="")
 
 print("89")
