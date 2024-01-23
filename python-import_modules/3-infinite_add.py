1c56c17571ed452153d52806320f28db12da9c20
Mottais <mottais.afy@outlook.fr>
2024-01-23 22:52:45 +0100

    Task3 a program that prints the result of the addition of all arguments

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+sum = 0
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            sum = sum + int(sys.argv[i])
+
+    print("{}".format(sum))
