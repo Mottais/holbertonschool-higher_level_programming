5458a795be1f28f85a4ea91a17919ec67ba8d361
Mottais <mottais.afy@outlook.fr>
2024-01-24 16:25:56 +0100

    Task8. ByteCode -> Python #3

---
+++ python-import_modules/102-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Test_Task102 = 102-magic_calculation car erreur avec 102-magic_calculation
+from Test_Task102 import magic_calculation
+a = 4
+b = 5
+print(magic_calculation(a, b))
+
