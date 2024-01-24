6282c744e479139e447b4cf5a6141dc6902139cf
Mottais <mottais.afy@outlook.fr>
2024-01-24 19:10:58 +0100

    Task6 Test autre solution

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
 
     from sys import argv
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
 
     from calculator_1 import add, sub, div, mul
