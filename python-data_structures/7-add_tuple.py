feb35d611ac7cfde1718076c29326ebbb7898a92
Mottais <mottais.afy@outlook.fr>
2024-01-29 19:11:16 +0100

    Gestion du cas où 1er tuple < 2 éléments

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    liste_c = list(tuple_a)
-    for i in range(2):
-        if i < len(tuple_b):
-            liste_c[i] = tuple_a[i] + tuple_b[i]
-        else:
-            liste_c[i] = tuple_a[i]
+    liste_c = [0, 0]
+    for i in range(len(tuple_a)):
+        liste_c[i] += tuple_a[i]
+    for i in range(len(tuple_b)):
+        liste_c[i] += tuple_b[i]
+
     return(tuple(liste_c))
