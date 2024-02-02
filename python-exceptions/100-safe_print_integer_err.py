9b468d710a4a7f58bc34c829e6233aaf0630fa64
Mottais <mottais.afy@outlook.fr>
2024-02-02 12:20:17 +0100

    mise sous forme de commantaire le test export vers fichier et correction message erreur

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,8 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        with open("./histo_err", "a") as file:
-            print("AAA: {}".format(e), file=file)
-        print("BBB: {}".format(e), file=sys.stderr)
+        # test export vers fichier
+        # with open("./histo_err", "a") as file:
+        #    print("Exception: {}".format(e), file=file)
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
