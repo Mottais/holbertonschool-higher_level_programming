71a8907f2593fb31a4bbe8e7addc6d366e42f1db
Mottais <mottais.afy@outlook.fr>
2024-02-06 18:22:00 +0100

    Retour à mon fichier .py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Defines say_my_name function"""
+"""
+say_my_name function :
+Prints 'My name is <first name> <last name>'
+TypeError ifthe first_name or last_name is not a string
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints 'My name is <first name> <last name>'
-        TypeError: The first_name or last_name is not a string
+    """
+    Prints 'My name is <first name> <last name>'
     """
     if not type(first_name) is str:
         raise TypeError("first_name must be a string")
