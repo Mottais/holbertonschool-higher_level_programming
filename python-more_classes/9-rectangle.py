2a9ae928da7865502392927e12074fdadd04e1ef
Mottais <mottais.afy@outlook.fr>
2024-02-08 23:21:28 +0100

    Test

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -15,14 +15,13 @@ class Rectangle:
 
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
-
         if type(width) is not int:
             raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         if width < 0:
             raise ValueError("width must be >= 0")
+        self.__height = height
         self.__width = width
         Rectangle.number_of_instances += 1
 
