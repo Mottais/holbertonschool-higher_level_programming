e9c5f7fdf9fb4c879ea89b1a609ffac8a5d4f037
Mottais <mottais.afy@outlook.fr>
2024-02-08 16:36:57 +0100

    Test code FlorainV

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,13 +10,14 @@ def text_indentation(text):
     """ Prints a text with 2 new lines
     after each of these characters: ., ? and :
     """
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
+
     i = 0
-    while i < len(text):
-        print(text[i], end='')
-        if text[i] in ".?:":
+    while i < (len(text)):
+        print(text[i], end="")
+        if text[i] in '.?:':
             print("\n")
-            while(i + 1 < len(text) and text[i + 1] == ' '):
+            while i + 1 < len(text) and text[i + 1] == ' ':
                 i += 1
         i += 1
