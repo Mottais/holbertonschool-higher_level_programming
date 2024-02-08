20b3278dddc014c4e9d1815c499aff60d0b2d3c9
Mottais <mottais.afy@outlook.fr>
2024-02-08 16:45:31 +0100

    Test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,8 +12,9 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
+
     i = 0
-    while i < len(text):
+    while i < (len(text)):
         print(text[i], end='')
         if text[i] in ".?:":
             print("\n")
