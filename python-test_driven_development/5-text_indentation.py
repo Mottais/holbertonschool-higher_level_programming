4b7f7dae7ea163f538bee71018f6673d1b074dc3
Mottais <mottais.afy@outlook.fr>
2024-02-08 16:50:25 +0100

    Retour a mon code avec espace apres while

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" Prints a text with 2 new lines
-after each of these characters: ., ? and :
-text must be a string,
-There should be no space at the beginning
-or at the end of each printed line"""
+"""
+this
+is a
+5 line comment
+"""
 
 
 def text_indentation(text):
-    """ Prints a text with 2 new lines
-    after each of these characters: ., ? and :
     """
-    if type(text) is not str:
+    indenting a text
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     i = 0
-    while i < (len(text)):
-        print(text[i], end="")
-        if text[i] in '.?:':
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ".?:":
             print("\n")
-            while i + 1 < len(text) and text[i + 1] == ' ':
+            while (i + 1 < len(text) and text[i + 1] == ' '):
                 i += 1
         i += 1
