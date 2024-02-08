ec649aacdeb23fb2027209594d561f332df76f73
Mottais <mottais.afy@outlook.fr>
2024-02-08 23:01:44 +0100

    Test

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -12,29 +12,39 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.height = height
-        self.width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__height
-
     @width.setter
     def width(self, value):
-        if not type(value) is int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        return self.__height
+
     @height.setter
     def height(self, value):
-        if not type(value) is int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
