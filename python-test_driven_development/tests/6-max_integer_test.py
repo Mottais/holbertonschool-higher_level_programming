fa9e8d71ec30c768f01259a33dbb7130323cbf24
Mottais <mottais.afy@outlook.fr>
2024-02-08 19:38:52 +0100

    test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -31,3 +31,19 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_mid(self):
         self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+
+    def test_5_max_liste_TypeError(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, "3", 4])
+        with self.assertRaises(TypeError):
+            print(max_integer([1, 3], (1, 2, 1)))
+        with self.assertRaises(TypeError):
+            print(max_integer({8, 2, 1}))
+
+    def test_6_max_liste_ZeroDivisionError(self):
+        with self.assertRaises(ZeroDivisionError):
+            max_integer([1, 2, 3, 4/0])
+
+    def test_7_max_liste_ValueError(self):
+        with self.assertRaises(ValueError):
+            max_integer([1, 2, 3, int("9" * 4301)])
