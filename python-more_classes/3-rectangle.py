b49b8e83e4b5f1afc501d91bce031242af688da5
Mottais <mottais.afy@outlook.fr>
2024-02-08 21:45:36 +0100

    Gestion du cas où self.__height == 0 or self.__width == 0

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -50,4 +50,6 @@ class Rectangle:
         return (self.__height + self.__width) * 2
 
     def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
         return (("#" * self.__width + '\n') * self.__height)[:-1]
