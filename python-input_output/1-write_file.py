06c3f93043406c9c1f86081cb3bc4127667301a9
Mottais <mottais.afy@outlook.fr>
2024-02-14 19:44:16 +0100

    Suppression du code qui m'a servit de test

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,3 @@ def write_file(filename="", text=""):
     """ function that writes a string to a text file (UTF8)  """
     with open(filename, "w", encoding='utf-8') as file:
         return file.write(text)
-
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
