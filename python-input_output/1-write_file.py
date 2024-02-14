6dfee8ac6fd271aedb7b20ff4c4c7736c56eaee6
Mottais <mottais.afy@outlook.fr>
2024-02-14 19:42:45 +0100

    Correction chemin où le fichier doit être créé

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
     with open(filename, "w", encoding='utf-8') as file:
         return file.write(text)
 
-nb_characters = write_file("./my_first_file.txt", "This School is so cool!\n")
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
 print(nb_characters)
