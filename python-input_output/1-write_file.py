ec53dcdd2e07a0a7b0bfea0872151474cb2cb515
Mottais <mottais.afy@outlook.fr>
2024-02-14 19:41:04 +0100

    Task1. Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Definition of write function"""
+
+
+def write_file(filename="", text=""):
+    """ function that writes a string to a text file (UTF8)  """
+    with open(filename, "w", encoding='utf-8') as file:
+        return file.write(text)
+
+nb_characters = write_file("./my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
