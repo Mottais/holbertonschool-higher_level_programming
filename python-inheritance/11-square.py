7610979b91c0ef3c792cb2d2502aca6142596cfd
Mottais <mottais.afy@outlook.fr>
2024-02-14 15:48:46 +0100

    Ajout self.integer_validator("size", size)

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     """Classe Square herite de la classe Rectangle"""
     def __init__(self, size):
         "teste et initialise l'attributs size"
+        self.integer_validator("size", size)
         Rectangle.__init__(self, size, size)
         self.__size = size
 
