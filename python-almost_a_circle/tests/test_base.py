0cc9a5ea570a046b19f09defc176978de6d0db68
Mottais <mottais.afy@outlook.fr>
2024-02-21 21:12:38 +0100

    Retour à mon fichier

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,83 +1,130 @@
 #!/usr/bin/python3
-import unittest, json
+import unittest
 from models.base import Base
-from models.rectangle import Rectangle
 from models.square import Square
+
 """Creating test cases for the base module"""
 
 
 class test_base(unittest.TestCase):
     """Testing base"""
 
-    def test_id_none(self):
-        """Sending no id"""
+    def test_init_(self):
+        """création d'un objet de la classe Base"""
         b = Base()
-        self.assertEqual(1, b.id)
-
-    def test_id(self):
-        """Sending a valid id"""
-        b = Base(50)
-        self.assertEqual(50, b.id)
-
-    def test_id_zero(self):
-        """Sending an id 0"""
-        b = Base(0)
-        self.assertEqual(0, b.id)
-
-    def test_id_negative(self):
-        """Sending a negative id"""
+        self.assertTrue(b.id == 1)
+        b = Base()
+        self.assertTrue(b.id == 2)
+        b = Base(1)
+        self.assertTrue(b.id == 1)
+        b = Base()
+        self.assertTrue(b.id == 3)
+        b = Base(4)
+        self.assertTrue(b.id == 4)
+        b = Base()
+        self.assertTrue(b.id == 4)
         b = Base(-20)
-        self.assertEqual(-20, b.id)
-
-    def test_id_string(self):
-        """Sending an id that is not an int"""
+        self.assertTrue(b.id == -20)
         b = Base("Betty")
-        self.assertEqual("Betty", b.id)
-
-    def test_id_list(self):
-        """Sending an id that is not an int"""
+        self.assertTrue(b.id == "Betty")
         b = Base([1, 2, 3])
-        self.assertEqual([1, 2, 3], b.id)
-
-    def test_id_dict(self):
-        """Sending an id that is not an int"""
+        self.assertTrue([1, 2, 3] == b.id)
         b = Base({"id": 109})
-        self.assertEqual({"id": 109}, b.id)
+        self.assertTrue({"id": 109} == b.id)
 
-    def test_id_tuple(self):
-        """Sending an id that is not an int"""
-        b = Base((8,))
-        self.assertEqual((8,), b.id)
+    def test_init_ZeroDivisionError(self):
+        """Creating a Base object with a division by zero"""
+        with self.assertRaises(ZeroDivisionError):
+            b = Base(1/0)
+            print(b)
 
-    def test_to_json_type(self):
+    def test_init_TypeError(self):
+        """Creating a Base object with a TypeError"""
+        with self.assertRaises(TypeError):
+            b = Base(1, 1)
+            print(b)
+
+    '''def test_init_ValueError(self):
+        """Creating a Base object with a TypeError"""
+        with self.assertRaises(ValueError):
+            b = Base(int("9" * 4301))
+            print(b)'''
+
+    def test_to_json_string(self):
         """Testing the json string"""
-        sq = Square(1)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([json_dict])
-        self.assertEqual(type(json_string), str)
 
-    def test_to_json_value(self):
-        """Testing the json string"""
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([json_dict])
-        self.assertEqual(
-            json.loads(json_string), [{"id": 609, "y": 0, "size": 1, "x": 0}]
-        )
+        Dict = {"toto": "tutu"}
+        json_string = Base.to_json_string([Dict, Dict])
+        self.assertTrue(json_string == '[{"toto": "tutu"}, {"toto": "tutu"}]')
+        self.assertTrue(type(json_string) is str)
 
-    def test_to_json_None(self):
-        """Testing the json string"""
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
+        json_string = Base.to_json_string(1)
+        self.assertTrue(json_string == '1')
+        self.assertTrue(type(json_string) is str)
+
         json_string = Base.to_json_string(None)
-        self.assertEqual(json_string, "[]")
+        self.assertTrue(json_string == '[]')
+        self.assertTrue(type(json_string) is str)
 
-    def test_to_json_Empty(self):
-        """Testing the json string"""
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
         json_string = Base.to_json_string([])
-        self.assertEqual(json_string, "[]")
+        self.assertTrue(json_string == '[]')
+        self.assertTrue(type(json_string) is str)
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_to_json_string_TypeError(self):
+        """using to_json_string with a TypeError"""
+        with self.assertRaises(TypeError):
+            json_string = Base.to_json_string()
+            print(json_string)
+
+    def test_from_json_string(self):
+        """Testing from_json_string"""
+
+        String_liste = '[{"toto": 1}, {"tutu": 2}]'
+        obj_from_sting = Base.from_json_string(String_liste)
+        self.assertTrue((obj_from_sting == [{"toto": 1}, {"tutu": 2}]))
+        self.assertTrue(type(obj_from_sting) is list)
+
+        String_liste = None
+        obj_from_sting = Base.from_json_string(String_liste)
+        self.assertTrue((obj_from_sting == []))
+        self.assertTrue(type(obj_from_sting) is list)
+
+        String_liste = '[]'
+        obj_from_sting = Base.from_json_string(String_liste)
+        self.assertTrue((obj_from_sting == []))
+        self.assertTrue(type(obj_from_sting) is list)
+
+        String_liste = '1'
+        obj_from_sting = Base.from_json_string(String_liste)
+        self.assertTrue((obj_from_sting != []))
+        self.assertTrue(type(obj_from_sting) is not list)
+
+    def test_from_json_string_TypeError(self):
+        """using from_json_string with a TypeError"""
+        with self.assertRaises(TypeError):
+            String = 2
+            obj_from_sting = Base.from_json_string(String)
+            print(obj_from_sting)
+
+    def test_save_to_file(self):
+        """Testing save_to_file"""
+
+        sq1 = Square(1)
+        sq2 = Square(2)
+        Base.save_to_file( [sq1, sq2])
+        with open("Base.json", "r") as file:
+            self.assertTrue(type(file.read()) is str)
+
+        Base.save_to_file(None)
+        with open("Base.json", "r") as file:
+            File_contain = (file.read())
+            self.assertTrue(File_contain == "[]")
+
+        Base.save_to_file([])
+        with open("Base.json", "r") as file:
+            File_contain = (file.read())
+            self.assertTrue(File_contain == "[]")
+
+
+if __name__ == '__main__':
+    unittest.main()
