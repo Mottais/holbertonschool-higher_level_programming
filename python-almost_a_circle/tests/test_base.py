02602dec81972c0a2ba124ec9ac3a350c547f837
Mottais <mottais.afy@outlook.fr>
2024-02-21 14:38:17 +0100

    Add tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -10,60 +10,65 @@ from models.square import Square
 class test_base(unittest.TestCase):
     """Testing base"""
 
-    def test_id_none(self):
+    def test_id_new_base(self):
         """Sending no id"""
         b = Base()
-        self.assertEqual(1, b.id)
-
-    def test_id(self):
-        """Sending a valid id"""
-        b = Base(50)
-        self.assertEqual(50, b.id)
+        self.assertTrue(b.id == 1)
+        b = Base()
+        self.assertTrue(b.id == 2)
+        b = Base(1)
+        self.assertTrue(b.id == 1)
+        b = Base()
+        self.assertTrue(b.id == 3)
+        b = Base(4)
+        self.assertTrue(b.id == 4)
+        b = Base()
+        self.assertTrue(b.id == 4)
 
     def test_id_zero(self):
         """Sending an id 0"""
         b = Base(0)
-        self.assertEqual(0, b.id)
+        self.assertTrue(0 == b.id)
 
     def test_id_negative(self):
         """Sending a negative id"""
         b = Base(-20)
-        self.assertEqual(-20, b.id)
+        self.assertTrue(-20 == b.id)
 
     def test_id_string(self):
         """Sending an id that is not an int"""
         b = Base("Betty")
-        self.assertEqual("Betty", b.id)
+        self.assertTrue("Betty" == b.id)
 
     def test_id_list(self):
         """Sending an id that is not an int"""
         b = Base([1, 2, 3])
-        self.assertEqual([1, 2, 3], b.id)
+        self.assertTrue([1, 2, 3] == b.id)
 
     def test_id_dict(self):
         """Sending an id that is not an int"""
         b = Base({"id": 109})
-        self.assertEqual({"id": 109}, b.id)
+        self.assertTrue({"id": 109} == b.id)
 
     def test_id_tuple(self):
         """Sending an id that is not an int"""
         b = Base((8,))
-        self.assertEqual((8,), b.id)
+        self.assertTrue((8,) == b.id)
 
     def test_to_json_type(self):
         """Testing the json string"""
         sq = Square(1)
         json_dict = sq.to_dictionary()
         json_string = Base.to_json_string([json_dict])
-        self.assertEqual(type(json_string), str)
+        self.assertTrue(type(json_string) is str)
 
     def test_to_json_value(self):
         """Testing the json string"""
         sq = Square(1, 0, 0, 609)
         json_dict = sq.to_dictionary()
         json_string = Base.to_json_string([json_dict])
-        self.assertEqual(
-            json.loads(json_string), [{"id": 609, "y": 0, "size": 1, "x": 0}]
+        self.assertTrue(
+            json.loads(json_string) == [{"id": 609, "y": 0, "size": 1, "x": 0}]
         )
 
     def test_to_json_None(self):
@@ -71,14 +76,14 @@ class test_base(unittest.TestCase):
         sq = Square(1, 0, 0, 609)
         json_dict = sq.to_dictionary()
         json_string = Base.to_json_string(None)
-        self.assertEqual(json_string, "[]")
+        self.assertTrue(json_string == "[]")
 
     def test_to_json_Empty(self):
         """Testing the json string"""
         sq = Square(1, 0, 0, 609)
         json_dict = sq.to_dictionary()
         json_string = Base.to_json_string([])
-        self.assertEqual(json_string, "[]")
+        self.assertTrue(json_string == "[]")
 
     if __name__ == '__main__':
         unittest.main()
