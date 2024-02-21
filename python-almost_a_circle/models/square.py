e8431c599c53e78ddc2b4a4f165e4d4f2b206f0a
Mottais <mottais.afy@outlook.fr>
2024-02-21 10:04:07 +0100

    Test fichier de Kevin

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
 """
 
-define a class inheriting from Rectangle
+A class Square that inherits from Rectangle
 
 """
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """define a class inheriting from Rectangle"""
+    """
+    A class Square that inherits from Rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square object.
-
-        Args:
-            size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position
-            y (int, optional): The y-coordinate of the square's position
-            id (int, optional): The unique identifier of the square
-        """
         super().__init__(size, size, x, y, id)
-        # ou bien
-        # Rectangle.__init__(self, size, size, x, y, id)
 
     @property
     def size(self):
-        """Get the size of the square."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Set the size of the square."""
-        if not type(value) is int:
+        """Set the size of the Square"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -39,27 +31,22 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
-        """Return a string representation of the Square."""
-        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
-                f"{self.width}")
+        """def in str format for print"""
+        return ("[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width))
 
     def update(self, *args, **kwargs):
-        """
-        Update value of Square object
-        """
+        """Update value of attribute"""
         if args:
-            if len(args) > 0:
-                self.id = args[0]
-                if len(args) > 1:
-                    self.size = args[1]
-                    if len(args) > 2:
-                        self.x = args[2]
-                        if len(args) > 3:
-                            self.y = args[3]
+            list_arg = ["id", "size", "x", "y"]
+            i = 0
+            for attr in args:
+                setattr(self, list_arg[i], attr)
+                i += 1
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Square."""
-        return dict(id=self.id, x=self.x, size=self.size, y=self.y)
+        """Returns the dictionary representation of a Square"""
+        return {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
