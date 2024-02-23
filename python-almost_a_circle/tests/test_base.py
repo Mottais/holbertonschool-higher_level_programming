06ba594ad8a6fa66445eacc794ec02d3a0a4a23f
Mottais <mottais.afy@outlook.fr>
2024-02-23 17:36:07 +0100

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -139,12 +139,14 @@ class test_base(unittest.TestCase):
             self.assertTrue((file.read()) == "[]")
 
         """Test saving a file into JSON format with an empty list"""
-        '''try:
+        try:
             os.remove("Square.json")
         except FileNotFoundError:
             pass
         Square.save_to_file([])
-        self.assertTrue(os.path.exists("Square.json"))'''
+        with open("Square.json", "r") as file:
+            self.assertTrue((file.read()) == "[]")
+        self.assertTrue(os.path.exists("Square.json"))
 
     def test_09_create(self):
         """Testing create"""
