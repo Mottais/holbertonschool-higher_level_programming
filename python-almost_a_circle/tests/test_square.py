1a38646f5244ac29a22a81b72be612bcbe25de87
Mottais <mottais.afy@outlook.fr>
2024-02-23 12:32:30 +0100

    test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -89,23 +89,14 @@ class test_square(unittest.TestCase):
 
 
 
-    '''def test_load_from_file_same_x(self):
-        """Checking that an object was created from the
-        list and has the same x"""
-        r1 = Square(10, 2, 8)
-        list_squares_input = [r1]
-        Square.save_to_file(list_squares_input)
-        list_squares_output = Square.load_from_file()
-        self.assertEqual(r1.x, list_squares_output[0].x)'''
-
-    def test_load_from_file_same_y(self):
+    '''def test_load_from_file_same_y(self):
         """Checking that an object was created from the
         list and has the same y"""
         r1 = Square(10, 2, 8)
         list_squares_input = [r1]
         Square.save_to_file(list_squares_input)
         list_squares_output = Square.load_from_file()
-        self.assertEqual(r1.y, list_squares_output[0].y)
+        self.assertEqual(r1.y, list_squares_output[0].y)'''
 
     def test_display_square(self):
         """Checking the stdout output by capturing it"""
