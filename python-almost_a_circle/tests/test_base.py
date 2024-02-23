2527f168a5305a1cdbc3622475ebbcfdb2579aac
Mottais <mottais.afy@outlook.fr>
2024-02-23 17:26:05 +0100

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -130,6 +130,10 @@ class test_base(unittest.TestCase):
         with open("Base.json", "r") as file:
             self.assertTrue((file.read()) == "[]")
 
+        Square.save_to_file([])
+        with open("Square.json", "r") as file:
+            self.assertTrue((file.read()) == "[]")
+
 
 
         """Test saving a file into JSON format with an empty list"""
