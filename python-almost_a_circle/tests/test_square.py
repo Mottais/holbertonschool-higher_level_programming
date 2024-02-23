68bfc75013f1e7669b267cb1a4bc1ce4a1746e40
Mottais <mottais.afy@outlook.fr>
2024-02-23 12:38:11 +0100

    test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -99,17 +99,7 @@ class test_square(unittest.TestCase):
         self.assertEqual(r1.y, list_squares_output[0].y)
 
 
-    '''def test_display_square_size_one(self):
-        """Checking the stdout output by capturing it"""
-        capturedOutput = StringIO()
-        sys.stdout = capturedOutput
-        r1 = Square(1)
-        r1.display()
-        sys.stdout = sys.__stdout__
-        output = "#\n"
-        self.assertEqual(capturedOutput.getvalue(), output)'''
-
-    def test_display_square_size_zero(self):
+    '''def test_display_square_size_zero(self):
         """Checking the stdout output by capturing it"""
         capturedOutput = StringIO()
         sys.stdout = capturedOutput
@@ -117,7 +107,7 @@ class test_square(unittest.TestCase):
         r1.display()
         sys.stdout = sys.__stdout__
         output = "###\n###\n###\n"
-        self.assertEqual(capturedOutput.getvalue(), output)
+        self.assertEqual(capturedOutput.getvalue(), output)'''
 
     def test_display_square(self):
         """Checking the stdout output by capturing it"""
