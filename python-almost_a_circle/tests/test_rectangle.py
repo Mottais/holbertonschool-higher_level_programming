df4d00ee7018e269ada64a3e2c50a42594f25367
Mottais <mottais.afy@outlook.fr>
2024-02-23 09:59:10 +0100

    test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -10,7 +10,7 @@ from io import StringIO
 
 class test_rectangle(unittest.TestCase):
     """Testing rectangle"""
-    def test_00_init_(self):
+    def test_01_init_(self):
         """Testing _init"""
         Base._Base__nb_objects = 0
         r1 = Rectangle(10, 2)
@@ -20,7 +20,25 @@ class test_rectangle(unittest.TestCase):
         self.assertTrue(r2.x == 3)
         self.assertTrue(r2.y == 4)
 
-    def test_02_area(self):
+    def test_02_init_TypeError(self):
+        """Creating a Rectangle object with a TypeError"""
+        with self.assertRaises(TypeError):
+            r = Rectangle("1", 1)
+            print(r)
+
+    def test_03_init_ValueError(self):
+        """Creating a Rectangle object with a ValueError"""
+        with self.assertRaises(ValueError):
+            r = Rectangle(-1, 1)
+            print(r)
+
+    def test_04_init_ZeroDivisionError(self):
+        """Creating a Rectangle object with a division by zero"""
+        with self.assertRaises(ZeroDivisionError):
+            r = Rectangle(1/0, 2)
+            print(r)
+
+    def test_05_area(self):
         """Testing area"""
         r1 = Rectangle(3, 2)
         self.assertEqual(r1.area(), 6)  # 3 * 2 = 6 (width * height)
