77adabe7ab2331380c50f242b4a6c567679e0592
Mottais <mottais.afy@outlook.fr>
2024-02-23 12:26:24 +0100

    test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -86,22 +86,12 @@ class test_square(unittest.TestCase):
 
 
 
-    '''def test_json_string(self):
-        """Testing that the json string gets converted into a list"""
-        list_input = [{"id": 2089, "size": 10}, {"id": 2712, "size": 7}]
-        json_list_input = Square.to_json_string(list_input)
-        list_output = Square.from_json_string(json_list_input)
-        s1 = {"id": 2089, "size": 10}
-        s2 = {"size": 7, "id": 2712}
-        self.assertEqual(list_input[0], s1)
-        self.assertEqual(list_input[1], s2)'''
-
-    def test_dict_to_instance(self):
+    '''def test_dict_to_instance(self):
         """test that an instance is created from a dict"""
         r1 = Square(5)
         r1_dictionary = r1.to_dictionary()
         r2 = Square.create(**r1_dictionary)
-        self.assertNotEqual(r1, r2)
+        self.assertNotEqual(r1, r2)'''
 
     def test_isnot_dict_to_instance(self):
         """test that an instance is created from a dict"""
