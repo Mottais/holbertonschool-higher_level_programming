0eba97847b17320d0149154b6c9eeae2fa889b99
Mottais <mottais.afy@outlook.fr>
2024-02-23 14:53:54 +0100

    test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -8,7 +8,7 @@ from io import StringIO
 
 class test_rectangle(unittest.TestCase):
     """Testing rectangle"""
-    '''def test_01_init_(self):
+    def test_01_init_(self):
         """Testing _init"""
         Base._Base__nb_objects = 0
         r1 = Rectangle(10, 2)
@@ -16,32 +16,32 @@ class test_rectangle(unittest.TestCase):
         self.assertTrue(r1.width == 10)
         self.assertTrue(r1.height == 2)
         self.assertTrue(r2.x == 3)
-        self.assertTrue(r2.y == 4)'''
+        self.assertTrue(r2.y == 4)
 
-    '''def test_02_init_TypeError(self):
+    def test_02_init_TypeError(self):
         """Creating a Rectangle object with a TypeError"""
         with self.assertRaises(TypeError):
             r = Rectangle("1", 1)
-            print(r)'''
+            print(r)
 
-    '''def test_03_init_ValueError(self):
+    def test_03_init_ValueError(self):
         """Creating a Rectangle object with a ValueError"""
         with self.assertRaises(ValueError):
             r = Rectangle(-1, 1)
-            print(r)'''
+            print(r)
 
-    '''def test_04_init_ZeroDivisionError(self):
+    def test_04_init_ZeroDivisionError(self):
         """Creating a Rectangle object with a division by zero"""
         with self.assertRaises(ZeroDivisionError):
             r = Rectangle(1/0, 2)
-            print(r)'''
+            print(r)
 
-    '''def test_05_area(self):
+    def test_05_area(self):
         """Testing area"""
         r1 = Rectangle(3, 2)
-        self.assertTrue(r1.area() == 6)'''
+        self.assertTrue(r1.area() == 6)
 
-    '''def test_06_display(self):
+    def test_06_display(self):
         """Testing display"""
         r1 = Rectangle(2, 3)
 
@@ -56,14 +56,14 @@ class test_rectangle(unittest.TestCase):
 
         # réinitialise la sortie standard à sa valeur par défaut.
         sys.stdout = sys.__stdout__
-        self.assertEqual(output.getvalue(), "##\n" * 3)'''
+        self.assertEqual(output.getvalue(), "##\n" * 3)
 
-    '''def test_07_str(self):
+    def test_07_str(self):
         """Testing __str__"""
         r1 = Rectangle(4, 6, 2, 1, 12)
-        self.assertTrue(str(r1) == "[Rectangle] (12) 2/1 - 4/6")'''
+        self.assertTrue(str(r1) == "[Rectangle] (12) 2/1 - 4/6")
 
-    '''def test_08_update(self):
+    def test_08_update(self):
         """Testing update"""
         r1 = Rectangle(10, 10, 10, 10)
         r1.update(89)
@@ -77,8 +77,8 @@ class test_rectangle(unittest.TestCase):
         r1.update(89, 2, 3, 4, 5)
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
         r1.update(89, 2, 3, 4, 5, 6)
-        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")'''
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
 
-    '''if __name__ == '__main__':
-        unittest.main()'''
+    if __name__ == '__main__':
+        unittest.main()
     pass
