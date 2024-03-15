bbc6decc066ade7f99dabebe73c4be5f236db458
Mottais <mottais.afy@outlook.fr>
2024-03-15 18:15:36 +0100

    Task16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- Liste la table qui a des nom valides (non vides ou non nuls) et les scores dans l'ordre décroissant
-SELECT id, score, name FROM second_table WHERE (name != '' AND name IS NOT NULL) ORDER BY score DESC;
+-- SELECT id, score, name FROM second_table WHERE (name != '' AND name IS NOT NULL) ORDER BY score DESC;
+
+-- Liste la table qui a des noms (non nuls) et les scores dans l'ordre décroissant
+SELECT id, score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
