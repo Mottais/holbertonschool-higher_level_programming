0cbcf4727f7a1449905363a5ce44535d24f069cf
Mottais <mottais.afy@outlook.fr>
2024-03-15 16:57:13 +0100

    Task10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- liste les lignes de la table second_table de la base de données active.
+-- en affichant dans la première colonne le score et dans la deuxième le nom,
+-- triées par score décroissant du score.
+SELECT score, name FROM second_table ORDER BY score DESC;
