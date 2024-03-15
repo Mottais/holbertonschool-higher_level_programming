5f12876fad3e161f2368f470a4c45144beffb8c8
Mottais <mottais.afy@outlook.fr>
2024-03-15 16:19:15 +0100

    Task : Seulement pour labase de données active.

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- liste toutes les lignes de la table first_table de la base de données hbtn_0c_0 de votre serveur MySQL.
-SELECT * FROM hbtn_0c_0.first_table;
+-- liste toutes les lignes de la table first_table dela base de données active.
+SELECT * FROM first_table;
