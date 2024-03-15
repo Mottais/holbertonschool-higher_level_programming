1ff8402d68d7105987d53f9d03d73f0dff0c2365
Mottais <mottais.afy@outlook.fr>
2024-03-15 15:57:43 +0100

    Task4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Créé une table first_table avec deux colonnes id (entier) et name (256 caracteres) dans la base de données active.
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+)
