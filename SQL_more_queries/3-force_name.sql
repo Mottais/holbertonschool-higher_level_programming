4649c274872d69270da6843c8b058b2f44df94f0
Mottais <mottais.afy@outlook.fr>
2024-03-17 13:10:21 +0100

    cré une table 'force_name' si elle n'existe pas

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
--- cré une table 'force_name' dans la base de données actuelle
+-- cré une table 'force_name' dans la base de données actuelle si elle n'existe pas
 -- avec les colonnes id (INT) et name (VARCHAR(256) NON NULL)
-CREATE TABLE force_name (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
