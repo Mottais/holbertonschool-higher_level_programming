d7c8e0a37f40f3ef17fd823ac55702a2eb5578dc
Mottais <mottais.afy@outlook.fr>
2024-03-17 12:26:43 +0100

    Task0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Affiche les prévilèges de l'utilisateur user_0d_1
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+-- Affiche les prévilèges de l'utilisateur user_0d_2
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
