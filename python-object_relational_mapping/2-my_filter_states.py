aab00c070dd54858c0f42c41c00be3bba708ec7a
Mottais <mottais.afy@outlook.fr>
2024-03-18 18:30:13 +0100

    Task2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module listing all states from the database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connexion = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    curs = connexion.cursor()
+    curs.execute("""SELECT * FROM states
+                 WHERE BINARY name LIKE '{}%'
+                 ORDER BY id;""".format(argv[4]))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    connexion.close()
