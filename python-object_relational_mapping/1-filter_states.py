d87d2137b2cd9f2f19c17d4e02d4ae058276b4b2
Mottais <mottais.afy@outlook.fr>
2024-03-18 17:45:55 +0100

    Task1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module listing all states from the database"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connexion = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    curseur = connexion.cursor()
+    curseur.execute("""
+        SELECT * FROM states
+        WHERE name LIKE 'N%'
+        ORDER BY id;
+                    """)
+    rows = curseur.fetchall()
+    for row in rows:
+        print(row)
+    curseur.close()
+    connexion.close()
