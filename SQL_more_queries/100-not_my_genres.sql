6fdda0f6c4199674cfce863673b44d6f80b60a6b
Mottais <mottais.afy@outlook.fr>
2024-03-19 17:17:25 +0100

    Solution avec seulement 2 SELECT

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,14 +1,23 @@
 -- Liste les genres de séries qui ne sont pas associés à la série "Dexter"
 
 -- Liste temporaire des genres de la série "Dexter"
-DROP TEMPORARY TABLE IF EXISTS Genres_de_Dexter;
-CREATE TEMPORARY TABLE Genres_de_Dexter AS
-SELECT name FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = show_id
-INNER JOIN tv_genres ON tv_genres.id = genre_id
-WHERE title = "Dexter"
-ORDER by title ASC;
+-- DROP TEMPORARY TABLE IF EXISTS Genres_de_Dexter;
+-- CREATE TEMPORARY TABLE Genres_de_Dexter AS
+-- SELECT name FROM tv_shows
+-- INNER JOIN tv_show_genres ON tv_shows.id = show_id
+-- INNER JOIN tv_genres ON tv_genres.id = genre_id
+-- WHERE title = "Dexter"
+
 
 -- Liste des genres de séries qui ne sont pas dans la liste temporaire
+-- SELECT name FROM tv_genres
+-- WHERE name NOT IN (SELECT name FROM Genres_de_Dexter ORDER BY name ASC)
+-- ORDER BY name ASC;
+
+-- Solution avec seulement 2 SELECT
 SELECT name FROM tv_genres
-WHERE name NOT IN (SELECT name FROM Genres_de_Dexter ORDER BY name ASC);
+WHERE name NOT IN (	SELECT name FROM tv_shows
+					INNER JOIN tv_show_genres ON tv_shows.id = show_id
+					INNER JOIN tv_genres ON tv_genres.id = genre_id
+					WHERE title = "Dexter")
+ORDER BY name ASC;
