a8414e07938a40387dae20d8d7c2bd63fdfa3a23
Mottais <mottais.afy@outlook.fr>
2024-05-21 13:01:17 +0200

    Task0

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,4 @@
+document.querySelector('header').setAttribute('style', 'color:#00FF00');
+// autres méthodes:
+// document.getElementsByTagName('header')[0].setAttribute('style', 'color:#00FF00');
+// document.querySelectorAll('header')[0].setAttribute('style', 'color:#00FF00');
